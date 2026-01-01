b9f9eab4f04eed3491b5b419abdbaba495ed9d1e
geetanshi-27 <geetanshi.malik27@gmail.com>
2026-01-01 22:19:24 +0530

    adding tailwind styling

--- api-app/src/App.jsx
+++ api-app/src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
+import { FaCloudRain } from "react-icons/fa"
+
 
 const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY
 
@@ -8,7 +10,8 @@ function App() {
   const [weather, setWeather] = useState(null)
   const [city, setCity] = useState('')
 
-  const myapifetch = () => {
+  const myapifetch = (e) => {
+    e.preventDefault()
     setWeather(null)
     axios.get("https://api.openweathermap.org/data/2.5/weather", {
       params: {
@@ -26,21 +29,31 @@ function App() {
 
   return (
     <>
-    
-    <h1>Weather App 🌩️</h1>
-    <form>
-      <label>enter city
-        <input 
-          // value={city}
+    <div className="flex flex-col items-center h-screen justify-center bg-[#fff9eb]">
+    <h1 className="text-3xl font-bold text-blue-400 italic my-6">Weather App 🌩️</h1>
+    <form onSubmit={myapifetch} className="text-center">
+      <label className="italic text-yellow-500">enter city
+        <br></br>
+        <input className="text-black border border-gray-400 rounded-md px-2"
+          value={city}
           onChange={(e)=>setCity(e.target.value)}/>
       </label>
+      <br></br>
+    <button className="my-6 bg-black text-white font-bold py-2 px-4 rounded"> FIND WEATHER </button>
     </form>
-
-
-    <button onClick={myapifetch}> CLICK ME</button>
-    {weather && weather.main && weather.main.temp && <div>hi the weather is {weather.main.temp}
+    {weather && weather.main && weather.main.temp && <div className="italic">the temperature is {weather.main.temp} in {weather.name}
       
       </div>}
+   
+    {weather && weather.main && weather.main.humidity && <div style={{ display:'flex'}} className="italic"> 
+       <FaCloudRain className="text-sky-300 text-2xl mx-2" />
+       humidity is {weather.main.humidity}% </div>}
+
+
+
+
+
+      </div>
     
 
     </>
